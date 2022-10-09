77411c629fa22eb69b09eb9a977b624c0fc3e2da
Amirhesam Pourhosein <amirhesamph81@gmail.com>
2022-10-09 12:31:19 +0330

    1401-07-17 - final merge

--- Sources/Infrastructure/MedicalOffice.Persistence/Repositories/RepoHelpers/DateTimeHelper.cs
+++ Sources/Infrastructure/MedicalOffice.Persistence/Repositories/RepoHelpers/DateTimeHelper.cs
@@ -12,24 +12,8 @@ public static class DateTimeHelper
         var month = calendar.GetMonth(dateTime);
         var day = calendar.GetDayOfMonth(dateTime);
 
-        return $"{year}/{month}/{day}";
-    }
+        var format = $"{year}/{month}/{day}";
 
-    public static string ToPersianTime(this DateTime dateTime)
-    {
-        PersianCalendar calendar = new();
-
-        var hour = calendar.GetHour(dateTime);
-        var minute = calendar.GetMinute(dateTime);
-
-        return $"{hour}:{minute}";
-    }
-
-    public static string ToPersianDateTime(this DateTime dateTime)
-    {
-        var datePart = dateTime.ToPersianDate();
-        var timePart = dateTime.ToPersianTime();
-
-        return $"{datePart} - {timePart}";
+        return format;
     }
 }
