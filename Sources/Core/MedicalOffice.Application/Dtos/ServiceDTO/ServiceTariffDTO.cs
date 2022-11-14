a1581bd08fb8087a8b5775d25288bd48d6b42b3c
Vahid Babaey <vahidbabaey47@gmail.com>
2022-11-14 10:22:02 +0330

    Adding ServiceTariff

---
+++ Sources/Core/MedicalOffice.Application/Dtos/ServiceDTO/ServiceTariffDTO.cs
@@ -0,0 +1,47 @@
+﻿using MedicalOffice.Application.Dtos.Common;
+using MedicalOffice.Domain.Entities;
+using MedicalOffice.Domain.Enums;
+
+namespace MedicalOffice.Application.Dtos.ServiceDTO;
+
+public class ServiceTariffDTO
+{
+
+    /// <summary>
+    /// آیدی مطب
+    /// </summary>
+    public Guid OfficeId { get; set; }
+    /// <summary>
+    /// آیدی بیمه
+    /// </summary>
+    public Guid? InsuranceId { get; set; }
+    /// <summary>
+    /// آیدی سزویس
+    /// </summary>
+    public Guid? ServiceId { get; set; }
+    /// <summary>
+    /// نوع خدمت
+    /// </summary>
+    public ServiceType? ServiceType { get; set; }
+    /// <summary>
+    /// مبلغ تعرفه
+    /// </summary>
+    public float TariffValue { get; set; }
+    /// <summary>
+    /// تعرفه داخلی
+    /// </summary>
+    public float InternalTariffValue { get; set; }
+    /// <summary>
+    /// ما به التفاوت
+    /// </summary>
+    public float Difference { get; set; }
+    /// <summary>
+    /// درصد بیمه
+    /// </summary>
+    public float InsurancePercent { get; set; }
+    /// <summary>
+    /// مبلغ اضافه به بیمه
+    /// </summary>
+    public float AdjunctPrice { get; set; }
+
+}
---
+++ Sources/Core/MedicalOffice.Application/Features/TariffFile/Handlers/Queries/GetAllInsuranceNamesQueryHandler.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using MediatR;
+using MedicalOffice.Application.Contracts.Infrastructure;
+using MedicalOffice.Application.Contracts.Persistence;
+using MedicalOffice.Application.Dtos.InsuranceDTO;
+using MedicalOffice.Application.Dtos.SpecializationDTO;
+using MedicalOffice.Application.Features.SpecializationFile.Requests.Queries;
+using MedicalOffice.Application.Features.TariffFile.Requests.Queries;
+using MedicalOffice.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalOffice.Application.Features.TariffFile.Handlers.Queries
+{
+
+    public class GetAllInsuranceNamesQueryHandler : IRequestHandler<GetAllInsuranceNamesQuery, List<InsuranceNamesDTO>>
+    {
+        private readonly IInsuranceRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        private readonly string _requestTitle;
+
+        public GetAllInsuranceNamesQueryHandler(IInsuranceRepository repository, IMapper mapper, ILogger logger)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _logger = logger;
+            _requestTitle = GetType().Name.Replace("QueryHandler", string.Empty);
+        }
+
+        public async Task<List<InsuranceNamesDTO>> Handle(GetAllInsuranceNamesQuery request, CancellationToken cancellationToken)
+        {
+            List<InsuranceNamesDTO> result = new();
+
+            Log log = new();
+
+            try
+            {
+                var insuranceNames = await _repository.GetAll();
+
+                result = _mapper.Map<List<InsuranceNamesDTO>>(insuranceNames);
+
+                log.Header = $"{_requestTitle} succeded";
+                log.Type = LogType.Success;
+            }
+            catch (Exception error)
+            {
+                log.Header = $"{_requestTitle} failed";
+                log.Messages.Add(error.Message);
+                log.Type = LogType.Error;
+            }
+
+            await _logger.Log(log);
+
+            return result;
+        }
+    }
+
+}
