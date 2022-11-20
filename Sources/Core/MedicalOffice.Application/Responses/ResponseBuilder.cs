06fb7c00e54334fb3e9f56e894016925d56ac782
p.hashemi <parastouhashemi@gmail.com>
2022-11-20 23:58:15 +0330

    feat: create response builder

---
+++ Sources/Core/MedicalOffice.Application/Responses/ResponseBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalOffice.Application.Responses
+{
+    public class ResponseBuilder
+    {
+        public BaseResponse Success(HttpStatusCode statusCode, string message, params object[] data)
+        {
+            return new() { StatusCode = statusCode, Success = true, StatusDescription = message, Data = data.ToList() };
+        }
+
+        public BaseResponse Faild(HttpStatusCode statusCode, string message, params string[] errors)
+        {
+            return new() { StatusCode = statusCode, Success = false, StatusDescription = message, Errors = errors.ToList() };
+        }
+    }
+}
