8c4e72bd64bb7382aa1cdce91e5ed979e6d49f45
p.hashemi <parastouhashemi@gmail.com>
2022-11-29 15:26:06 +0330

    feat: add permission per route static class to constants to use in permission attribute

--- Sources/Api/MedicalOffice.WebApi/CustomAttributes/PermissionAttribute.cs
+++ Sources/Api/MedicalOffice.WebApi/CustomAttributes/PermissionAttribute.cs
@@ -3,41 +3,45 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using MedicalOffice.Application.Contracts.Persistence;
 using Microsoft.AspNetCore.WebUtilities;
+using MedicalOffice.Application.Constants;
 
 namespace MedicalOffice.WebApi.Attributes
 {
     public class PermissionAttribute : TypeFilterAttribute
     {
-        public PermissionAttribute(string[] Permission) : base(typeof(PermissionFilter))
+        public PermissionAttribute(string Permissions) : base(typeof(PermissionFilter))
         {
-            Arguments = new object[] { Permission };
+            Arguments = new object[] { Permissions };
         }
     }
 
     public class PermissionFilter : IAuthorizationFilter
     {
-        readonly string[] _permission;
+        readonly string _permissions;
         private readonly IPermissionRepository _repository;
 
         public PermissionFilter(
-            string[] permission,
+            string permissions,
             IPermissionRepository repository)
         {
-            _permission = permission;
+            _permissions = permissions;
             _repository = repository;
         }
 
         public async void OnAuthorization(AuthorizationFilterContext context)
         {
+            string[] separatingStrings = { ",", " " };
+            string[] permissions = _permissions.Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries);
+
             var userId = context.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's UserId
             var Roles = context.HttpContext.User.FindAll(ClaimTypes.Role).ToList();
-            if (!Roles.Any(x => x.Value == "Admin"))
+            if (!Roles.Any(x => x.Value == AdminRole.Name))
             {
                 var officeId = QueryHelpers.ParseQuery(context.HttpContext.Request.QueryString.Value)
 
                     .ToDictionary(x => x.Key, x => x.Value)["officeId"];
 
-                var hasPermission = await _repository.UserHasPermission(Guid.Parse(userId), Guid.Parse(officeId), _permission);
+                var hasPermission = await _repository.UserHasPermission(Guid.Parse(userId), Guid.Parse(officeId), permissions);
                 if (!hasPermission)
                 {
                     context.Result = new ForbidResult();
