587c02d3e26f265d405d05ee3d7a699503b6e4ae
p.hashemi <parastouhashemi@gmail.com>
2022-11-30 13:40:36 +0330

    feat: implement scheduling types enums

---
+++ Sources/Core/MedicalOffice.Domain/Enums/SchedulingTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalOffice.Domain.Enums
+{
+    public enum SchedulingTypes
+    {
+        Confirmed = 1,
+
+        BetweenPatients,
+        
+        Canceled,
+        
+        FreeTime,
+
+        FinalApproval
+    }
+}
