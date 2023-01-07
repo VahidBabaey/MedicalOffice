621527a709509688c6638275f2e876158d23caca
Vahid Babaey <vahidbabaey47@gmail.com>
2023-01-07 17:58:12 +0330

    Validation

--- Sources/Core/MedicalOffice.Application/Dtos/ServiceDTO/Validators/AddServiceValidator.cs
+++ Sources/Core/MedicalOffice.Application/Dtos/ServiceDTO/Validators/AddServiceValidator.cs
@@ -1,12 +1,18 @@
 ﻿using FluentValidation;
+using MedicalOffice.Application.Contracts.Persistence;
+using MedicalOffice.Application.Dtos.Common.CommonValidators;
+using MedicalOffice.Application.Dtos.Commons;
 
 namespace MedicalOffice.Application.Dtos.ServiceDTO.Validators;
 
 public class AddServiceValidator : AbstractValidator<ServiceDTO>
 {
-    public AddServiceValidator()
+    private readonly ISectionRepository _sectionRepository;
+    public AddServiceValidator(ISectionRepository sectionRepository)
     {
+        _sectionRepository = sectionRepository;
         RuleFor(x => x.Name).NotEmpty().Length(1, 200);
         RuleFor(x => x.GenericCode).NotEmpty();
+        Include(new SectionIdValidator(_sectionRepository));
     }
 }
