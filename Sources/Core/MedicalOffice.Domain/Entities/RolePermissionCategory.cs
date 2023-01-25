422adbf17115351db6b0dd3287ad7327b7e13bbf
p.hashemi <parastouhashemi@gmail.com>
2023-01-25 17:30:01 +0330

    feat: add rolePermission class

---
+++ Sources/Core/MedicalOffice.Domain/Entities/RolePermissionCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalOffice.Domain.Entities
+{
+    public class RolePermissionCategory
+    {
+        public Guid PermissionCategoryId { get; set; }
+        public Guid RoleId { get; set; }
+        public PermissionCategory PermissionCategory { get; set; }
+        public Role Role { get; set; }
+    }
+}
