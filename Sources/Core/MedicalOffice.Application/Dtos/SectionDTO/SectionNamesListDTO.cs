7345dcf329611b666a8b07021a8bf1da1f1cf91c
Vahid Babaey <vahidbabaey47@gmail.com>
2023-01-30 18:46:19 +0330

    Refactor: Remove all take/skips and specializationId in Service, add SectionNames api and return medicalstaff titles

---
+++ Sources/Core/MedicalOffice.Application/Dtos/SectionDTO/SectionNamesListDTO.cs
@@ -0,0 +1,11 @@
+﻿using MedicalOffice.Application.Dtos.Common;
+
+namespace MedicalOffice.Application.Dtos.SectionDTO;
+
+public class SectionNamesListDTO: BaseDto<Guid>
+{
+    /// <summary>
+    /// نام بخش
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+}
---
+++ Sources/Core/MedicalOffice.Domain/Enums/MedicalStaffTitle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalOffice.Domain.Enums
+{
+    /// <summary>
+    /// عنوان
+    /// </summary>
+     public enum Title
+    {
+        /// <summary>
+        /// جناب آقای دکتر
+        /// </summary>
+        MrDoctor = 1,
+        /// <summary>
+        /// سرکار خانم دکتر
+        /// </summary>
+        MrsDoctor = 2,
+    }
+}
