b7c29ef54934f1deee8008f5d0d6f55df9d3e53b
Vahid Babaey <vahidbabaey47@gmail.com>
2023-01-30 16:12:18 +0330

    Refactor Search in medicalStaff

---
+++ Sources/Core/MedicalOffice.Application/Features/MedicalStaffFile/Handler/Queries/GetMedicalStaffBySearchQueryHandler.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using MediatR;
+using MedicalOffice.Application.Contracts.Infrastructure;
+using MedicalOffice.Application.Contracts.Persistence;
+using MedicalOffice.Application.Dtos.MedicalStaffDTO;
+using MedicalOffice.Application.Dtos.SectionDTO;
+using MedicalOffice.Application.Features.MedicalStaffFile.Request.Queries;
+using MedicalOffice.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalOffice.Application.Features.MedicalStaffFile.Handler.Queries
+{
+
+    public class GetMedicalStaffBySearchQueryHandler : IRequestHandler<GetMedicalStaffBySearch, List<MedicalStaffListDTO>>
+    {
+        private readonly IMedicalStaffRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        private readonly string _requestTitle;
+
+        public GetMedicalStaffBySearchQueryHandler(IMedicalStaffRepository repository, IMapper mapper, ILogger logger)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _logger = logger;
+            _requestTitle = GetType().Name.Replace("QueryHandler", string.Empty);
+        }
+
+        public async Task<List<MedicalStaffListDTO>> Handle(GetMedicalStaffBySearch request, CancellationToken cancellationToken)
+        {
+            List<MedicalStaffListDTO> result = new();
+
+            Log log = new();
+
+            try
+            {
+                var medicalStaff = await _repository.GetMedicalStaffBySearch(request.Name);
+
+                result = _mapper.Map<List<MedicalStaffListDTO>>(medicalStaff.Where(p => p.OfficeId == request.OfficeId));
+
+                log.Header = $"{_requestTitle} succeded";
+                log.Type = LogType.Success;
+            }
+            catch (Exception error)
+            {
+                log.Header = $"{_requestTitle} failed";
+                log.AdditionalData = error.Message;
+                log.Type = LogType.Error;
+            }
+
+            await _logger.Log(log);
+
+            return result;
+        }
+    }
+
+}
