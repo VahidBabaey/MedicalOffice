7901ced702a95bb92527f8aeb1c6950b96c6ac15
p.hashemi <parastouhashemi@gmail.com>
2023-01-31 11:06:56 +0330

    fix: recommend identity performance issue

--- Sources/Infrastructure/MedicalOffice.Persistence/Repositories/UserRepository.cs
+++ Sources/Infrastructure/MedicalOffice.Persistence/Repositories/UserRepository.cs
@@ -20,7 +20,19 @@ namespace MedicalOffice.Persistence.Repositories
 
         public async Task<User?> CheckByPhoneOrNationalId(string phoneNumber, string nationalId)
         {
-            var user = await _dbContext.Users.SingleAsync(x => x.PhoneNumber == phoneNumber || x.NationalID == nationalId);
+            var user = await _dbContext.Users.SingleOrDefaultAsync(x => x.PhoneNumber == phoneNumber || x.NationalID == nationalId);
+
+            if (user != null)
+            {
+                return user;
+            }
+
+            return null;
+        }
+
+        public async Task<User?> FindExistAndActiveUser(string phoneNumber, bool isActive)
+        {
+            var user = await _dbContext.Users.SingleOrDefaultAsync(x => x.PhoneNumber == phoneNumber && x.IsActive == isActive);
 
             if (user != null)
             {
