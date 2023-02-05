2ba5fcd152274722e113232b9353f077f1d3ed0a
p.hashemi <parastouhashemi@gmail.com>
2023-02-05 21:48:31 +0330

    chore: update userOfficeRoleConfiguration

--- Sources/Infrastructure/MedicalOffice.Persistence/Configurations/Entities/UserOfficeRoleConfiguration.cs
+++ Sources/Infrastructure/MedicalOffice.Persistence/Configurations/Entities/UserOfficeRoleConfiguration.cs
@@ -26,8 +26,7 @@ namespace MedicalOffice.Persistence.Configurations.Entities
                     new UserOfficeRole()
                     {
                         UserId = Guid.Parse("EAEF7EDD-C18A-4CCE-A450-72EE26C18A8D"),
-                        RoleId = Guid.Parse("aca86b1a-8e36-4467-9e3c-2f826822df10"),
-                        OfficeId = Guid.Parse("40dcd9d7-4765-4aa4-ae98-287108b608b0")
+                        RoleId = Guid.Parse("aca86b1a-8e36-4467-9e3c-2f826822df10")
                     },
                     new UserOfficeRole()
                     {
@@ -37,14 +36,12 @@ namespace MedicalOffice.Persistence.Configurations.Entities
                     },
                     new UserOfficeRole()
                     {
-                        Id = Guid.Parse("14a55dca-9465-423e-a62b-0280d47710f5\r\n"),
                         UserId = Guid.Parse("EAEF7EDD-C18A-4CCE-A450-72EE26C18A8D"),
                         RoleId = Guid.Parse("70508b44-eae8-4d40-9318-651ae5b38f40"),
                         OfficeId = Guid.Parse("40dcd9d7-4765-4aa4-ae98-287108b608b0")
                     },
                     new UserOfficeRole()
                     {
-                        Id = Guid.Parse("57200e72-5790-4d33-9323-2007f8975e93"),
                         UserId = Guid.Parse("EAEF7EDD-C18A-4CCE-A450-72EE26C18A8D"),
                         RoleId = Guid.Parse("8c07113f-ec06-4db0-90c7-e1d292525c7c"),
                         OfficeId = Guid.Parse("40dcd9d7-4765-4aa4-ae98-287108b608b0")
