056e666b546ccea1999ff82ba585d00c9164ef37
p.hashemi <parastouhashemi@gmail.com>
2023-02-25 19:29:08 +0330

    fix: fix issue of add and update membership

--- Sources/Core/MedicalOffice.Application/Dtos/MembershipDTO/Validators/UpdateMembershipValidator.cs
+++ Sources/Core/MedicalOffice.Application/Dtos/MembershipDTO/Validators/UpdateMembershipValidator.cs
@@ -7,8 +7,7 @@ public class UpdateMembershipValidator : AbstractValidator<UpdateMembershipDTO>
     public UpdateMembershipValidator()
     {
         RuleFor(x => x.Name).NotEmpty().Length(1, 100);
-        RuleFor(x => x.IsActive);
-        RuleFor(x => x.Discount).NotEmpty();
+        RuleFor(x => x.IsActive).NotNull().Must(x => x == false || x == true);
         RuleFor(x => x.Discount).NotEmpty().LessThanOrEqualTo(100);
     }
 }
