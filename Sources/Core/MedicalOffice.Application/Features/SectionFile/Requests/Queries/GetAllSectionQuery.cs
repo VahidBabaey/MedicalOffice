7b4ed1163d3ded9b30c3fc9985ac3aede91a95c4
Vahid Babaey <vahidbabaey47@gmail.com>
2023-03-04 13:40:31 +0330

    Adding Ordering

--- Sources/Core/MedicalOffice.Application/Features/SectionFile/Requests/Queries/GetAllSectionQuery.cs
+++ Sources/Core/MedicalOffice.Application/Features/SectionFile/Requests/Queries/GetAllSectionQuery.cs
@@ -2,6 +2,7 @@
 using MedicalOffice.Application.Dtos.Common;
 using MedicalOffice.Application.Dtos.SectionDTO;
 using MedicalOffice.Application.Responses;
+using MedicalOffice.Domain.Enums;
 
 namespace MedicalOffice.Application.Features.SectionFile.Requests.Queries;
 
@@ -9,4 +10,5 @@ public class GetAllSectionQuery : IRequest<BaseResponse>
 {
     public ListDto Dto { get; set; } = new ListDto();
     public Guid OfficeId { get; set; }
+    public Order? Order { get; set; }
 }
---
+++ Sources/Core/MedicalOffice.Domain/Enums/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace MedicalOffice.Domain.Enums;
+
+/// <summary>
+/// ترتیب نمایش
+/// </summary>
+public enum Order
+{
+    /// <summary>
+    /// قدیمی ترین
+    /// </summary>
+    OldRecords = 0,
+
+    /// <summary>
+    /// جدیدترین
+    /// </summary>
+    NewRecords = 1,
+
+}
