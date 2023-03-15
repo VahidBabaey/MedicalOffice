3d5b524b5616ec4f4520ed400568a16b24ee8964
p.hashemi <parastouhashemi@gmail.com>
2023-03-15 16:40:44 +0330

    fix: issue of extra character

--- Sources/Infrastructure/MedicalOffice.Persistence/Repositories/MedicalStaffRepository.cs
+++ Sources/Infrastructure/MedicalOffice.Persistence/Repositories/MedicalStaffRepository.cs
@@ -53,7 +53,7 @@ public class MedicalStaffRepository : GenericRepository<MedicalStaff, Guid>, IMe
                 IsTechnicalAssistant = item.IsTechnicalAssistant,
                 IsReferrer = item.IsReferrer,
                 IsSpecialist = item.IsSpecialist,
-                CreatedDate = item.CreatedDate,`
+                CreatedDate = item.CreatedDate,
             };
             medicalStaffListDTOs.Add(medicalStaffListDTO);
         }
